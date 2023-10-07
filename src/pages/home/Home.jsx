c5fa514117da0702f11b9ac423913c1eaca0cc92
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-10-07 14:07:35 +0600

    react router, tailwindcss, daisy ui, firebase setup and header created

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../shared/Header";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/shared/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold text-center mt-8">Celebration Hub</h1>
+            <h3 className="text-center text-2xl mt-3">Crafting Moments, Making Memories: Your Event, Our Expertise.</h3>
+        </div>
+    );
+};
+
+export default Header;
